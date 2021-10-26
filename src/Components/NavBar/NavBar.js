e9424becffb74e300b627e2a8daa5f766a3198da
Logan <loganed09@gmail.com>
2021-10-26 14:42:14 -0400

    converted NavBar into function

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 
-export class NavBar extends React.Component {
-  render() {
-    return (
-    <nav>
+
+// export class NavBar extends React.Component {
+  
+//   const [navbarOpen, setNavbarOpen] = useState(false);
+
+//   render() {
+//     return (
+//     <nav className="navbar">
+//         <a href="#about">About Me</a>
+//         <a href="#projects">Projects</a>
+//         <h2>Logan Edwards</h2>
+//         <a href="#contact">Contact</a>
+//         <a href="#">Something</a>
+//         <div className="dropdown">
+//           <button className="mobile-id" onClick="dropDown()">
+//               <h2 className="mobile-id">LE</h2>
+//           </button>
+//           <div className="dropdown-content">
+//             <a href="#about">About Me</a>
+//             <a href="#projects">Projects</a>
+//             <a href="#contact">Contact</a>
+//             <a href="#">Something</a>
+//           </div>
+//         </div>
+//     </nav>
+//     )
+//   }
+// }
+
+
+export default function NavBar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
         <a href="#about">About Me</a>
         <a href="#projects">Projects</a>
         <h2>Logan Edwards</h2>
         <a href="#contact">Contact</a>
         <a href="#">Something</a>
-        <button class="mobile-id">
-            <h2 class="mobile-id">LE</h2>
-        </button>
+        <div className="dropdown">
+          <button className="mobile-id">
+              <h2 className="mobile-id">LE</h2>
+          </button>
+          <div className="dropdown-content">
+            <a href="#about">About Me</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+            <a href="#">Something</a>
+          </div>
+        </div>
     </nav>
     )
-  }
 }
