72a80799d57b5efff3d02217a72ae2ffb21dae2d
Logan <loganed09@gmail.com>
2021-10-27 13:01:21 -0400

    updated hamburger menu functionality and added About component files

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -42,11 +42,14 @@ export default function NavBar() {
   return (
     <nav className="navbar">
         <a className="navItem nitem1" href="#projects">Projects</a>
-        <h2 className="navItem nitem2">Logan Edwards</h2>
+        <h2 className="navItem nitem2">LOGAN EDWARDS</h2>
         <a className="navItem nitem3" href="#contact">Contact</a>
+        <p className={`initials ${navbarOpen ? " hideInitials" : ""}`}>
+            <a href="#about">LE</a>
+        </p>
         <div className="dropdown">
           <button className="mobile-id" onClick={handleToggle}>
-              LE
+              <i class="fas fa-bars"></i>
           </button>
           <ul className={`dropdown-content ${navbarOpen ? " showMenu" : ""}`}>
             <a href="#projects">Projects</a>
